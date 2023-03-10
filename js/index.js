8df46b2199eca62c34db74c9629da02c420a6b54
Henrique Damasceno <87776413+Henriquim25@users.noreply.github.com>
2023-03-10 03:52:28 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,55 @@
+import { Controls } from "./controls.js";
+import { Timer } from "./timer.js";
+import { elements } from "./elements.js";
+
+const {
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonStop,
+  minutesDisplay,
+  secondsDisplay  
+} = elements
+
+let minutes = Number(minutesDisplay.textContent);
+
+const controls = Controls({
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonStop,
+});
+
+const timer = Timer({
+  minutesDisplay,
+  secondsDisplay,
+  resetControls: controls.reset,
+  minutes,
+});
+
+buttonPlay.addEventListener("click", function () {
+  controls.play();
+  timer.hold()
+  timer.countdown();
+});
+buttonPause.addEventListener("click", function () {
+  controls.pause();
+  timer.hold()
+});
+
+buttonStop.addEventListener("click", function () {
+  controls.reset();
+  timer.reset();
+});
+
+buttonSet.addEventListener("click", function () {
+  let newMinutes = controls.getMinutes()
+  if (!newMinutes) {
+    timer.reset();
+    return;
+  }
+
+  minutes = newMinutes;
+  timer.updateDisplay(minutes, 0);
+  timer.updateMinutes(minutes)
+});
